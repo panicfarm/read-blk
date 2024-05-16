3f5949c11bc724760a15d7a83f0db73d45618402
Alec Matusis <matusis@matusis.com>
2024-05-16 14:08:42 -0700

    Added reader of blocks inside a .blk file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use bitcoin::block::Block;
+use bitcoin::consensus::Decodable;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
@@ -13,13 +15,31 @@ fn main() {
                 let mut contents = Vec::new();
                 file.read_to_end(&mut contents).unwrap();
                 println!("File {}: {} bytes", file_name, contents.len());
+                read_blocks(&contents);
             }
-            Err(_) => {
+            Err(err) => {
                 // file not found, assume it's the last file
-                println!("last File {}", file_name);
+                println!("err {}", err);
                 break;
             }
         }
         file_num += 1;
+    }
+}
+
+fn read_blocks(file_bytes: &Vec<u8>) {
+    let mut i = 0;
+    loop {
+        if i == file_bytes.len() {
+            break;
+        }
+
+        assert_eq!(&file_bytes[i..i + 4], &[0xf9, 0xbe, 0xb4, 0xd9]);
+        let len = u32::from_le_bytes(file_bytes[i + 4..i + 8].try_into().unwrap()) as usize;
+        let bytes = &file_bytes[i + 8..i + 8 + len];
+        let block = Block::consensus_decode(&mut bytes.to_vec().as_slice()).unwrap();
+        println!("read block {:?}", block.block_hash());
+
+        i += 8 + len;
     }
 }

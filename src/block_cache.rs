bc1e9836e3f68d4837bbc515390642383e1aea87
Vitaly S <vitalys357@gmail.com>
2024-06-17 11:08:44 -0700

    switched to bitcoin::BlockHash
    added usage of BlockCache to main

--- src/block_cache.rs
+++ src/block_cache.rs
@@ -1,24 +1,26 @@
+use bitcoin::BlockHash;
 use std::collections::{HashMap, HashSet};
+use std::str::FromStr;
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
-pub struct BlockHash(String);
+//#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+//pub struct BlockHash(String);
 
 #[derive(Debug, Clone)]
-struct Block {
-    height: u32,
-    hash: BlockHash,
+pub struct BlockInfo {
+    pub hash: BlockHash,
     prev_hash: BlockHash,
 }
 
 #[derive(Debug)]
-struct BlockCache {
-    out_of_order_blocks: HashMap<BlockHash, Vec<Block>>,
+pub struct BlockCache {
+    pending_full_blocks: HashMap<BlockHash, bitcoin::block::Block>,
+    out_of_order_blocks: HashMap<BlockHash, Vec<BlockInfo>>,
     staged_blocks: StagedBlocks,
 }
 
 #[derive(Debug, Clone)]
 struct TreeNode {
-    block: Block,
+    block_info: BlockInfo,
     parent: Option<BlockHash>,
     children: HashSet<BlockHash>,
     orig_level: u32,
@@ -26,16 +28,25 @@ struct TreeNode {
 
 #[derive(Debug)]
 struct StagedBlocks {
-    block_tree_root: Option<BlockHash>,
-    block_nodes: HashMap<BlockHash, TreeNode>,
-    block_tree_depth: u32,
-    block_tree_roots_removed_cnt: u32,
+    tree_root: Option<BlockHash>,
+    nodes: HashMap<BlockHash, TreeNode>,
+    tree_depth: u32,
+    root_removed_cnt: u32,
+}
+
+impl BlockInfo {
+    pub fn new(hash: &BlockHash, prev_hash: &BlockHash) -> Self {
+        BlockInfo {
+            hash: hash.clone(),
+            prev_hash: prev_hash.clone(),
+        }
+    }
 }
 
 impl TreeNode {
-    fn new(block: Block) -> Self {
+    fn new(block_info: BlockInfo) -> Self {
         TreeNode {
-            block,
+            block_info,
             parent: None,
             children: HashSet::new(),
             orig_level: 0,
@@ -44,91 +55,102 @@ impl TreeNode {
 }
 
 impl BlockCache {
-    fn new() -> Self {
+    pub fn new() -> Self {
         BlockCache {
+            pending_full_blocks: HashMap::new(),
             out_of_order_blocks: HashMap::new(),
             staged_blocks: StagedBlocks::new(),
         }
     }
 
-    fn add_block(&mut self, block: &Block) {
-        if self.staged_blocks.block_tree_root.is_none()
+    pub fn add_block(&mut self, block: bitcoin::block::Block) {
+        let block_info = BlockInfo::new(&block.block_hash(), &block.header.prev_blockhash);
+        self.pending_full_blocks.insert(block_info.hash, block);
+        self.add_block_info(&block_info);
+    }
+
+    fn add_block_info(&mut self, block_info: &BlockInfo) {
+        if self.staged_blocks.tree_root.is_none()
             || self
                 .staged_blocks
-                .block_nodes
-                .get(&block.prev_hash)
+                .nodes
+                .get(&block_info.prev_hash)
                 .is_some()
         {
-            self.staged_blocks.add_block(block);
-            //dbg!("added {}", &block.hash.0);
-            self.move_out_of_order_blocks_to_staged(&block.hash);
+            self.staged_blocks.add_block_info(&block_info);
+            //dbg!("added {}", &block_info.hash.to_string());
+            self.move_out_of_order_blocks_to_staged(&block_info.hash);
         } else {
             self.out_of_order_blocks
-                .entry(block.prev_hash.clone())
+                .entry(block_info.prev_hash.clone())
                 .or_default()
-                .push(block.clone());
+                .push(block_info.clone());
         }
     }
 
     fn move_out_of_order_blocks_to_staged(&mut self, prev_hash: &BlockHash) {
-        if let Some(block_vec) = self.out_of_order_blocks.remove(prev_hash) {
-            for block in block_vec {
-                self.staged_blocks.add_block(&block);
-                //dbg!("added {}", &block.hash.0);
-                self.move_out_of_order_blocks_to_staged(&block.hash);
+        if let Some(block_info_vec) = self.out_of_order_blocks.remove(prev_hash) {
+            for block_info in block_info_vec {
+                self.staged_blocks.add_block_info(&block_info);
+                //dbg!("added {}", &block_info.hash.to_string());
+                self.move_out_of_order_blocks_to_staged(&block_info.hash);
             }
         }
     }
 
-    /// when the depth in the whole tree reaches threshold, the root block in the tree can migrate to the main chain
-    fn remove_root_if_ready(&mut self, depth_threshold: u32) -> Option<Block> {
-        self.staged_blocks.remove_root_if_ready(depth_threshold)
+    /// when the depth in the whole tree reaches threshold, the root block_info in the tree can migrate to the main chain
+    pub fn remove_block_if_ready(&mut self, depth_threshold: u32) -> Option<bitcoin::Block> {
+        if let Some(block_info) = self
+            .staged_blocks
+            .remove_block_info_if_ready(depth_threshold)
+        {
+            self.pending_full_blocks.remove(&block_info.hash)
+        } else {
+            None
+        }
     }
 }
 
 impl StagedBlocks {
     fn new() -> Self {
         StagedBlocks {
-            block_tree_root: None,
-            block_nodes: HashMap::new(),
-            block_tree_depth: 0,
-            block_tree_roots_removed_cnt: 0,
+            tree_root: None,
+            nodes: HashMap::new(),
+            tree_depth: 0,
+            root_removed_cnt: 0,
         }
     }
 
-    fn add_block(&mut self, block: &Block) {
-        let mut new_node = TreeNode::new(block.clone());
-        if self.block_tree_root.is_none() {
+    fn add_block_info(&mut self, block_info: &BlockInfo) {
+        let mut new_node = TreeNode::new(block_info.clone());
+        if self.tree_root.is_none() {
             // if this the tree is empty, this is the first root node
             new_node.orig_level = 1;
-            self.block_tree_root = Some(block.hash.clone());
-            self.block_nodes.insert(block.hash.clone(), new_node);
+            self.tree_root = Some(block_info.hash.clone());
+            self.nodes.insert(block_info.hash.clone(), new_node);
         } else {
             let parent_node = self
-                .block_nodes
-                .get_mut(&new_node.block.prev_hash)
+                .nodes
+                .get_mut(&new_node.block_info.prev_hash)
                 .expect("parent node expected");
             new_node.orig_level = parent_node.orig_level + 1;
-            new_node.parent = Some(parent_node.block.hash.clone());
-            parent_node.children.insert(block.hash.clone());
-            let depth = new_node.orig_level - self.block_tree_roots_removed_cnt;
-            self.block_nodes.insert(block.hash.clone(), new_node);
-            if self.block_tree_depth < depth {
-                self.block_tree_depth = depth;
+            new_node.parent = Some(parent_node.block_info.hash.clone());
+            parent_node.children.insert(block_info.hash.clone());
+            let depth = new_node.orig_level - self.root_removed_cnt;
+            self.nodes.insert(block_info.hash.clone(), new_node);
+            if self.tree_depth < depth {
+                self.tree_depth = depth;
             }
         }
     }
 
-    fn remove_root_if_ready(&mut self, depth_threshold: u32) -> Option<Block> {
-        if self.block_tree_depth < depth_threshold {
+    fn remove_block_info_if_ready(&mut self, depth_threshold: u32) -> Option<BlockInfo> {
+        if self.tree_depth < depth_threshold {
             return None;
         }
 
-        let root_hash = self.block_tree_root.as_ref().expect("root hash expected");
-        let root_node = self
-            .block_nodes
-            .remove(root_hash)
-            .expect("root node expected");
+        let root_hash = self.tree_root.as_ref().expect("root hash expected");
+        let root_node = self.nodes.remove(root_hash).expect("root node expected");
         let mut new_root_node_opt = None;
         let mut losing_children_opt = None;
         if root_node.children.len() > 1 {
@@ -143,7 +165,7 @@ impl StagedBlocks {
                 }
             }
             let winning_child_hash = child_hash_with_deepest_subtree.expect("child hash expected");
-            new_root_node_opt = self.block_nodes.get_mut(&winning_child_hash);
+            new_root_node_opt = self.nodes.get_mut(winning_child_hash);
 
             let mut losing_children = root_node.children.clone();
             losing_children.remove(winning_child_hash);
@@ -151,34 +173,34 @@ impl StagedBlocks {
         } else {
             assert_eq!(root_node.children.len(), 1);
             for child_hash in root_node.children.iter() {
-                new_root_node_opt = self.block_nodes.get_mut(&child_hash);
+                new_root_node_opt = self.nodes.get_mut(child_hash);
             }
         }
 
         let new_root_node = new_root_node_opt.expect("new root node expected");
         new_root_node.parent = None;
-        self.block_tree_root = Some(new_root_node.block.hash.clone());
-        self.block_tree_depth -= 1;
-        self.block_tree_roots_removed_cnt += 1;
+        self.tree_root = Some(new_root_node.block_info.hash.clone());
+        self.tree_depth -= 1;
+        self.root_removed_cnt += 1;
 
         if let Some(losing_children) = losing_children_opt {
             // remove losing branches
             self.purge_nodes(&losing_children);
         }
 
-        Some(root_node.block)
+        Some(root_node.block_info)
     }
 
     fn purge_nodes(&mut self, block_hashes: &HashSet<BlockHash>) {
         for hash in block_hashes.iter() {
-            let node = self.block_nodes.remove(&hash).expect("node expected");
+            let node = self.nodes.remove(hash).expect("node expected");
             self.purge_nodes(&node.children);
         }
     }
 
     fn calculate_depth_from_node(&self, block_hash: &BlockHash) -> u32 {
         let mut max_depth = 0;
-        let node = self.block_nodes.get(block_hash).expect("node expected");
+        let node = self.nodes.get(block_hash).expect("node expected");
         for child_hash in node.children.iter() {
             let depth = self.calculate_depth_from_node(child_hash);
             if depth > max_depth {
@@ -193,11 +215,14 @@ impl StagedBlocks {
 mod tests {
     use super::*;
 
-    fn create_block(height: u32, hash: &str, prev_hash: &str) -> Block {
-        Block {
-            height,
-            hash: BlockHash(hash.to_string()),
-            prev_hash: BlockHash(prev_hash.to_string()),
+    fn create_block_hash(hash: &str) -> BlockHash {
+        BlockHash::from_str(&hash.repeat(64)).unwrap()
+    }
+
+    fn create_block_info(hash: &str, prev_hash: &str) -> BlockInfo {
+        BlockInfo {
+            hash: create_block_hash(&hash),
+            prev_hash: create_block_hash(&prev_hash),
         }
     }
 
@@ -207,84 +232,83 @@ mod tests {
 
         // Create an unbalanced tree with branches and a deepest branch of 10 levels
         /*
-                              A
+                              0
                              / \
-                            B   C
+                            1   2
                            /   / \
-                          D   E   F
+                          3   4   5
                          /   /     \
-                        G   H       J
+                        6   7       8
                        /     \
-                      I       K
+                      9       A
                                \
-                                L
+                                B
                                  \
-                                  M
+                                  C
         */
         let blocks = vec![
-            create_block(0, "A", ""),  // Level 0
-            create_block(4, "J", "F"), // Level 4, out of order
-            create_block(2, "E", "C"), // Level 2, out of order
-            create_block(2, "F", "C"), // Level 2, out of order
-            create_block(1, "B", "A"), // Level 1
-            create_block(1, "C", "A"), // Level 1
-            create_block(4, "K", "H"), // Level 4, out of order
-            create_block(3, "H", "E"), // Level 3, out of order
-            create_block(4, "I", "G"), // Level 4, out of order
-            create_block(2, "D", "B"), // Level 2
-            create_block(3, "G", "D"), // Level 3
-            create_block(5, "L", "K"), // Level 5
-            create_block(6, "M", "L"), // Level 6
+            create_block_info("0", "0"), // Level 0
+            create_block_info("8", "5"), // Level 4, out of order
+            create_block_info("4", "2"), // Level 2, out of order
+            create_block_info("5", "2"), // Level 2, out of order
+            create_block_info("1", "0"), // Level 1
+            create_block_info("2", "0"), // Level 1
+            create_block_info("A", "7"), // Level 4, out of order
+            create_block_info("7", "4"), // Level 3, out of order
+            create_block_info("9", "6"), // Level 4, out of order
+            create_block_info("3", "1"), // Level 2
+            create_block_info("6", "3"), // Level 3
+            create_block_info("B", "A"), // Level 5
+            create_block_info("C", "B"), // Level 6
         ];
 
         // Add blocks to the tree
-        for block in &blocks {
-            block_cache.add_block(block);
+        for block_info in &blocks {
+            block_cache.add_block_info(block_info);
         }
         //dbg!(&block_cache);
         assert!(block_cache.out_of_order_blocks.is_empty());
-        assert_eq!(block_cache.staged_blocks.block_tree_depth, 7);
+        assert_eq!(block_cache.staged_blocks.tree_depth, 7);
 
-        let expected_roots = vec!["A", "C", "E"];
+        let expected_roots = vec!["0", "2", "4"];
         for expected_root in expected_roots {
-            if let Some(removed_block) = block_cache.remove_root_if_ready(4) {
+            if let Some(removed_block) = block_cache.staged_blocks.remove_block_info_if_ready(4) {
                 assert_eq!(
-                    removed_block.hash.0, expected_root,
+                    &removed_block.hash,
+                    &create_block_hash(expected_root),
                     "expected root: {}, but got: {:?}",
-                    expected_root, removed_block.hash
+                    expected_root,
+                    removed_block.hash
                 );
             } else {
                 println!("expected to remove root but none was removed");
             }
         }
-        assert_eq!(block_cache.staged_blocks.block_tree_depth, 4);
+        assert_eq!(block_cache.staged_blocks.tree_depth, 4);
+        assert_eq!(block_cache.staged_blocks.root_removed_cnt, 3);
         let root_hash = block_cache
             .staged_blocks
-            .block_tree_root
+            .tree_root
             .as_ref()
             .expect("root hash expected");
         assert_eq!(
-            root_hash.0, "H",
-            "expected root: H, but got: {:?}",
-            root_hash
-        );
-        assert_eq!(
-            root_hash.0, "H",
-            "expected root: H, but got: {:?}",
+            root_hash,
+            &create_block_hash("7"),
+            "expected root: 7, but got: {:?}",
             root_hash
         );
         let node = block_cache
             .staged_blocks
-            .block_nodes
-            .get(&BlockHash("K".to_string()))
+            .nodes
+            .get(&create_block_hash("A"))
             .expect("node expected");
         assert_eq!(node.orig_level, 5);
         assert_eq!(
             node.parent.as_ref().expect("parent expected"),
-            &BlockHash("H".to_string())
+            &create_block_hash("7")
         );
         let mut children = HashSet::new();
-        children.insert(BlockHash("L".to_string()));
+        children.insert(create_block_hash("B"));
         assert_eq!(&node.children, &children);
         //dbg!(&block_cache);
     }
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,19 @@ mod block_cache;
 
 use bitcoin::block::Block;
 use bitcoin::consensus::Decodable;
-use hex::encode;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
+struct Importer {
+    block_cache: block_cache::BlockCache,
+}
+
 fn main() {
     let dir_path = "/home/ghost/dat/bitcoin/blocks/"; //bitcoin core leveldb
-    let mut file_num = 0;
     let mut file_num = 1328;
+    let mut importer = Importer::new();
     loop {
         let file_name = format!("blk{:05}.dat", file_num);
         let file_path = Path::new(dir_path).join(&file_name);
@@ -19,7 +23,7 @@ fn main() {
                 let mut contents = Vec::new();
                 file.read_to_end(&mut contents).unwrap();
                 println!("File {}: {} bytes", file_name, contents.len());
-                read_blocks(contents);
+                importer.read_blocks(contents);
             }
             Err(err) => {
                 // file not found, assume it's the last file
@@ -31,35 +35,47 @@ fn main() {
     }
 }
 
-fn read_blocks(file_bytes: Vec<u8>) {
-    let mut i = 0;
-    loop {
-        if i == file_bytes.len() {
-            break;
+impl Importer {
+    fn new() -> Self {
+        Importer {
+            block_cache: block_cache::BlockCache::new(),
         }
+    }
 
-        let len = u32::from_le_bytes(file_bytes[i + 4..i + 8].try_into().unwrap()) as usize;
-        println!("read {} {}", i, len);
-        if len > 0 {
-            let bytes = &file_bytes[i + 8..i + 8 + len];
-            assert_eq!(
-                &file_bytes[i..i + 4],
-                &[0xf9, 0xbe, 0xb4, 0xd9],
-                "{}, {}, {}",
-                i,
-                len,
-                hex::encode(&bytes),
-            );
-            let block = Block::consensus_decode(&mut bytes.to_vec().as_slice()).unwrap();
-            println!(
-                "read block {:?} {} header: work {} prev_hash {:?}",
-                block.block_hash(),
-                block.bip34_block_height().unwrap_or(0),
-                block.header.work(),
-                block.header.prev_blockhash
-            );
+    fn read_blocks(&mut self, file_bytes: Vec<u8>) {
+        let mut i = 0;
+        loop {
+            if i == file_bytes.len() {
+                break;
+            }
+
+            let len = u32::from_le_bytes(file_bytes[i + 4..i + 8].try_into().unwrap()) as usize;
+            println!("read {} {}", i, len);
+            if len > 0 {
+                let bytes = &file_bytes[i + 8..i + 8 + len];
+                assert_eq!(
+                    &file_bytes[i..i + 4],
+                    &[0xf9, 0xbe, 0xb4, 0xd9],
+                    "{}, {}, {}",
+                    i,
+                    len,
+                    hex::encode(&bytes),
+                );
+                let block = Block::consensus_decode(&mut bytes.to_vec().as_slice()).unwrap();
+                println!(
+                    "read block {:?} {} header: work {} prev_hash {:?}",
+                    block.block_hash(),
+                    block.bip34_block_height().unwrap_or(0),
+                    block.header.work(),
+                    block.header.prev_blockhash
+                );
+                self.block_cache.add_block(block);
+                if let Some(_block_ready_to_import) = self.block_cache.remove_block_if_ready(100) {
+                    //TODO add to main chain
+                }
+            }
+
+            i += 8 + len;
         }
-
-        i += 8 + len;
     }
 }
